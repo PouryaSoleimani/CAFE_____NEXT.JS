831a2743fcfdfaf0ce13fd76a24abfac83833924
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 13:50:26 +0330

    DESIGN

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,16 +12,16 @@ export default function HomePage() {
       {/* <!-- Navbar Start --> */}
       <div className="container-fluid p-0 nav-bar">
         <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3">
-          <Link href="index.html" className="navbar-brand px-lg-4 m-0">
+          <Link href="/" className="navbar-brand px-lg-4 m-0">
             <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
           </Link>
           <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+          <div className="navbar-collapse justify-content-between" id="navbarCollapse">
             <div className="navbar-nav ml-auto p-4">
               <Link href="/" className="nav-item nav-link active">Home</Link>
-              <Link href="/about" className="nav-item nav-link">About</Link>
+              <Link href="/about-us" className="nav-item nav-link">About</Link>
               <Link href="/services" className="nav-item nav-link">Service</Link>
               <Link href="/menu" className="nav-item nav-link">Menu</Link>
               <div className="nav-item dropdown">
@@ -31,7 +31,7 @@ export default function HomePage() {
                   <Link href="/testimonial" className="dropdown-item">Testimonial</Link>
                 </div>
               </div>
-              <Link href="contact.html" className="nav-item nav-link">Contact</Link>
+              <Link href="contact-us" className="nav-item nav-link">Contact</Link>
             </div>
           </div>
         </nav>
