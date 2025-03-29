5f3664bbe678f75ed6c19d05ef3c3a786470cb1b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 12:45:01 +0330

    START

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="antialiased">
+      <body className="antialiased font-[family-name:var(--font-geist-sans)]">
         <Main />
         <NextScript />
       </body>
