dbdf78a781a7d9257b5711beeefa656f1ffe680c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 15:29:52 +0330

    DESIGN

---
+++ components/modules/PageHeader/PageHeaderComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageHeaderComponent = () => {
+    return (
+        <div>PageHeaderComponent</div>
+    )
+}
+
+export default PageHeaderComponent
---
+++ components/modules/ServiceItem/ServiceItemCardComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceItemCardComponent = () => {
+    return (
+        <div>ServiceItemCardComponent</div>
+    )
+}
+
+export default ServiceItemCardComponent
