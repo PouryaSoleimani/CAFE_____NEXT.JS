bb6f03d0012e5cad4063faadbe57f383df6ab674
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 15:31:37 +0330

    DESIGN

---
+++ components/templates/Index/AboutUsTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUsTemplate = () => {
+    return (
+        <div>AboutUsTemplate</div>
+    )
+}
+
+export default AboutUsTemplate
---
+++ components/templates/Index/SliderTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SliderTemplate = () => {
+    return (
+        <div>SliderTemplate</div>
+    )
+}
+
+export default SliderTemplate
