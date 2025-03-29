5227aa374e0b5ce7f2c03affb3762ad6e82074b6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 16:01:46 +0330

    DESIGN

--- components/modules/Navbar/NavbarComponent.tsx
+++ components/modules/Navbar/NavbarComponent.tsx
@@ -1,26 +1,35 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 const NavbarComponent = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
     return (
         <div className="container-fluid p-0 nav-bar">
-            <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3 space-between">
+            <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3">
                 <Link href="/" className="navbar-brand px-lg-4 m-0">
                     <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
                 </Link>
-                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                    <span className="navbar-toggler-icon"> <GiHamburgerMenu className="w-6 h-6 m-auto translate-y-1" /> </span>
+                <button
+                    type="button"
+                    className="navbar-toggler"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <span className="navbar-toggler-icon">
+                        <GiHamburgerMenu className="w-6 h-6 m-auto translate-y-1" />
+                    </span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div className={`navbar-collapse ${isOpen ? 'show' : ''}`}>
                     <div className="navbar-nav ml-auto p-4">
                         <Link href="/" className="nav-item nav-link active">Home</Link>
                         <Link href="/about-us" className="nav-item nav-link">About</Link>
                         <Link href="/services" className="nav-item nav-link">Services</Link>
                         <Link href="/menu" className="nav-item nav-link">Menu</Link>
                         <div className="nav-item dropdown">
-                            <Link href="/" className="nav-link dropdown-toggle" data-toggle="dropdown">Pages</Link>
-                            <div className="dropdown-menu text-capitalize">
+                            <div className="nav-link dropdown-toggle" onClick={() => setDropdownOpen(!dropdownOpen)} role="button"  > Pages</div>
+                            <div className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}>
                                 <Link href="/reservation" className="dropdown-item">Reservation</Link>
                                 <Link href="/testimonial" className="dropdown-item">Testimonial</Link>
                             </div>
