1a457f3f27baa9677df56ee52dc7ba1c0fafb166
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 18:17:48 +0330

    SHAD____CN

--- components/modules/Navbar/NavbarComponent.tsx
+++ components/modules/Navbar/NavbarComponent.tsx
@@ -27,6 +27,7 @@ const NavbarComponent = () => {
                         <Link href="/menu" className="nav-item nav-link">Menu</Link>
                         <div className="nav-item dropdown">
                             <div className="nav-link dropdown-toggle" onClick={() => setDropdownOpen(!dropdownOpen)} role="button"  > Pages</div>
+
                             <div className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}>
                                 <Link href="/reservation" className="dropdown-item">Reservation</Link>
                                 <Link href="/testimonial" className="dropdown-item">Testimonial</Link>
