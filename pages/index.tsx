a26ce0d51c61a60b925bd0f29fb2dfc451f7cb6c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 15:48:28 +0330

    DESIGN

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaAward, FaCoffee, FaEnvelope, FaFacebookSquare, FaLinkedin, FaMapMarkerAlt, FaPhone, FaTruck, FaTwitter } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { RiCalendarScheduleFill } from "react-icons/ri";
 
 
@@ -13,14 +14,14 @@ export default function HomePage() {
     <>
       {/* <!-- Navbar Start --> */}
       <div className="container-fluid p-0 nav-bar">
-        <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3">
+        <nav className="navbar navbar-expand-lg bg-none navbar-dark py-3 space-between">
           <Link href="/" className="navbar-brand px-lg-4 m-0">
             <h1 className="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
           </Link>
           <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"> <GiHamburgerMenu className="w-6 h-6 m-auto translate-y-1" /> </span>
           </button>
-          <div className="navbar-collapse justify-content-between" id="navbarCollapse">
+          <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
             <div className="navbar-nav ml-auto p-4">
               <Link href="/" className="nav-item nav-link active">Home</Link>
               <Link href="/about-us" className="nav-item nav-link">About</Link>
