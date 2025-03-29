479cbf66b95e853ff63114d866470c26a57919bc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 14:08:45 +0330

    DESIGN

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
+// ^ HOME PAGE =================================================================================================================================================
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"], });
 
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"], });
 
+//  COMPONENT ================================================================================================================================================
 export default function HomePage() {
   return (
     <>
