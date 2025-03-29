e3a771a269e4d5491446f6fb305dcc7cda8aab27
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-29 15:40:13 +0330

    DESIGN

---
+++ components/templates/ReservationPage/ReservationDetailsTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReservationDetailsTemplate = () => {
+    return (
+        <div>ReservationDetailsTemplate</div>
+    )
+}
+
+export default ReservationDetailsTemplate
---
+++ components/templates/TestimonialPage/TestimonialCommentsTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestimonialCommentsTemplate = () => {
+    return (
+        <div>TestimonialCommentsTemplate</div>
+    )
+}
+
+export default TestimonialCommentsTemplate
