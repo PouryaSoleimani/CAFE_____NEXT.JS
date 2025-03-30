059800991bff7e4c0e7cf9ea4e818fc2f7bc4674
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-30 09:49:59 +0330

    SHAD____CN

--- components/modules/Swiper/SwiperComponent.tsx
+++ components/modules/Swiper/SwiperComponent.tsx
@@ -17,8 +17,8 @@ const SwiperComponent = () => {
                     <Image className="w-100 " src="/img/carousel-1.jpg" alt="Image" width={3000} height={3000} />
                     <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <h2 className="text-primary font-weight-medium m-0 ">We Have Been Serving</h2>
-                        <h1 className="display-1 text-white m-0  animate__animated  animate__fadeInUp animate__delay-2s animate__slow">COFFEE</h1>
-                        <h2 className="text-white m-0  animate__animated  animate__fadeInUp animate__delay-2s animate__slow">* SINCE 1950 *</h2>
+                        <h1 className="display-1 text-white m-0 ">COFFEE</h1>
+                        <h2 className="text-white m-0 ">* SINCE 1950 *</h2>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
