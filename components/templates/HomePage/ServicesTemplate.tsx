71a3e9ff7cedc72d9447fc6a458eda35a59a11da
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-30 19:02:30 +0330

    SHAD____CN

--- components/templates/HomePage/ServicesTemplate.tsx
+++ components/templates/HomePage/ServicesTemplate.tsx
@@ -1,8 +1,70 @@
+import Image from 'next/image'
 import React from 'react'
+import { FaAward, FaCoffee, FaTruck } from 'react-icons/fa'
+import { RiCalendarScheduleFill } from 'react-icons/ri'
 
 const ServicesTemplate = () => {
     return (
-        <div>ServicesTemplate</div>
+        <div className="container-fluid pt-5">
+            <div className="container">
+                <div className="section-title">
+                    <h4 className="text-primary text-uppercase" style={{ letterSpacing: "5px" }}>Our Services</h4>
+                    <h1 className="display-4">Fresh &amp;Organic Beans</h1>
+                </div>
+                <div className="row mx-auto">
+                    <div className="col-lg-6 mb-5">
+                        <div className="row align-items-center">
+                            <div className="col-sm-5">
+                                <div className="img-fluid mb-3 mb-sm-0">
+                                    <Image src="/img/service-1.jpg" alt="pic" width={300} height={300} />
+                                </div>
+                            </div>
+                            <div className="col-sm-7">
+                                <h4><span className="fa fa-truck service-icon"><FaTruck /></span> Fastest Door Delivery</h4>
+                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 mb-5">
+                        <div className="row align-items-center">
+                            <div className="col-sm-5">
+                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-2.jpg" alt="pic" width={300} height={300} />
+                            </div>
+                            <div className="col-sm-7">
+                                <h4><span className="fa fa-coffee service-icon"><FaCoffee /></span> Fresh Coffee Beans</h4>
+                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 mb-5">
+                        <div className="row align-items-center">
+                            <div className="col-sm-5">
+                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-3.jpg" alt="pic" width={300} height={300} />
+                            </div>
+                            <div className="col-sm-7">
+                                <h4><span className="fa fa-award service-icon"><FaAward /></span> Best Quality Coffee</h4>
+                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 mb-5">
+                        <div className="row align-items-center">
+                            <div className="col-sm-5">
+                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-4.jpg" alt="pic" width={300} height={300} />
+                            </div>
+                            <div className="col-sm-7">
+                                <h4><span className="fa fa-table service-icon"><RiCalendarScheduleFill /></span> Online Table Booking</h4>
+                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- components/templates/HomePage/SliderTemplate.tsx
+++ components/templates/HomePage/SliderTemplate.tsx
@@ -1,8 +1,78 @@
-import React from 'react'
+// ^SWIPER COMPONENT ========================================================================================================================================
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import Image from 'next/image';
+import styles from './../../modules/Swiper/SwiperComponent.module.css';
 
+// COMPONENT ================================================================================================================================================
 const SliderTemplate = () => {
     return (
-        <div>SliderTemplate</div>
+        <div className={styles.swiperContainer}>
+            <Swiper navigation={true} modules={[Navigation, Autoplay, EffectFade]} className="mySwiper carousel-inner max-h-[100vh]" loop={true} autoplay={{ delay: 5000, disableOnInteraction: false, }} effect="fade" speed={800} pagination={{ clickable: true }}   >
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-1.jpg" alt="Image" width={2000} height={2200} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0 ">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0 ">COFFEE</h1>
+                        <h2 className="text-white m-0 ">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh] '>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-2.jpg" alt="Image" width={2000} height={2000} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/caousel-3.webp" alt="Image" width={2000} height={2000} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-4.webp" alt="Image" width={2000} height={2500} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-5.jpg" alt="Image" width={2000} height={2000} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-6.webp" alt="Image" width={2000} height={2000} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className='h-[100vh]'>
+                    <Image className="w-100 h-[100vh]" src="/img/carousel-7.webp" alt="Image" width={2000} height={2000} />
+                    <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 className="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 className="display-1 text-white m-0">COFFEE</h1>
+                        <h2 className="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+
+            <div className="overlay-bottom"></div>
+        </div>
     )
 }
 
