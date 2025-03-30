cbc327df3d89357130543f27fedbb90be512d9fb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-30 17:43:06 +0330

    SHAD____CN

---
+++ components/templates/AboutUsPage/AboutUsPage.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const AboutUsTemplate = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default AboutUsTemplate
--- components/templates/AboutUsPage/StoryTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StoryTemplate = () => {
-    return (
-        <div>StoryTemplate</div>
-    )
-}
-
-export default StoryTemplate
