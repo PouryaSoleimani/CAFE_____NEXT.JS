e88a0a8ae42cbbb2568020d134cc975d871e11e9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-30 19:11:52 +0330

    SHAD____CN

--- components/modules/ServiceItem/ServiceItemCardComponent.tsx
+++ components/modules/ServiceItem/ServiceItemCardComponent.tsx
@@ -1,8 +1,26 @@
+// ^ SERVICE ITEM CARD COMPONENT =============================================================================================================
+import Image from 'next/image'
 import React from 'react'
+interface ServiceItemCardComponentProps { imageSRC: string; iconName: React.ElementType; serviceTitle: string; }
 
-const ServiceItemCardComponent = () => {
+
+const ServiceItemCardComponent: React.FC<ServiceItemCardComponentProps> = ({ imageSRC, iconName, serviceTitle }) => {
     return (
-        <div>ServiceItemCardComponent</div>
+        <div className="col-lg-6 mb-5 hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer">
+            <div className="bg-dark rounded p-4 d-flex flex-column justify-content-center align-items-center h-100">
+            <div className="row align-items-center">
+                <div className="col-sm-5">
+                    <div className="img-fluid mb-3 mb-sm-0">
+                        <Image src={imageSRC} alt="pic" width={300} height={300} />
+                    </div>
+                </div>
+                <div className="col-sm-7">
+                    <h4><span className="fa fa-truck service-icon">{React.createElement(iconName)}</span> {serviceTitle}</h4>
+                    <p className="m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iure nam sequi, quaerat ad hic tempore ea culpa quisquam, ab eaque aut voluptas perspiciatis beatae atque sed quibusdam aliquam est?</p>
+                </div>
+            </div>
+            </div>
+        </div>
     )
 }
 
--- components/templates/HomePage/ServicesTemplate.tsx
+++ components/templates/HomePage/ServicesTemplate.tsx
@@ -1,3 +1,4 @@
+import ServiceItemCardComponent from '@/components/modules/ServiceItem/ServiceItemCardComponent'
 import Image from 'next/image'
 import React from 'react'
 import { FaAward, FaCoffee, FaTruck } from 'react-icons/fa'
@@ -12,58 +13,10 @@ const ServicesTemplate = () => {
                     <h1 className="display-4">Fresh &amp;Organic Beans</h1>
                 </div>
                 <div className="row mx-auto">
-
-                    <div className="col-lg-6 mb-5">
-                        <div className="row align-items-center">
-                            <div className="col-sm-5">
-                                <div className="img-fluid mb-3 mb-sm-0">
-                                    <Image src="/img/service-1.jpg" alt="pic" width={300} height={300} />
-                                </div>
-                            </div>
-                            <div className="col-sm-7">
-                                <h4><span className="fa fa-truck service-icon"><FaTruck /></span> Fastest Door Delivery</h4>
-                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-5">
-                        <div className="row align-items-center">
-                            <div className="col-sm-5">
-                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-2.jpg" alt="pic" width={300} height={300} />
-                            </div>
-                            <div className="col-sm-7">
-                                <h4><span className="fa fa-coffee service-icon"><FaCoffee /></span> Fresh Coffee Beans</h4>
-                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-5">
-                        <div className="row align-items-center">
-                            <div className="col-sm-5">
-                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-3.jpg" alt="pic" width={300} height={300} />
-                            </div>
-                            <div className="col-sm-7">
-                                <h4><span className="fa fa-award service-icon"><FaAward /></span> Best Quality Coffee</h4>
-                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 mb-5">
-                        <div className="row align-items-center">
-                            <div className="col-sm-5">
-                                <Image className="/img-fluid mb-3 mb-sm-0" src="/img/service-4.jpg" alt="pic" width={300} height={300} />
-                            </div>
-                            <div className="col-sm-7">
-                                <h4><span className="fa fa-table service-icon"><RiCalendarScheduleFill /></span> Online Table Booking</h4>
-                                <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                    invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                            </div>
-                        </div>
-                    </div>
-
+                    <ServiceItemCardComponent imageSRC={"/img/service-1.jpg"} iconName={FaTruck} serviceTitle={"Fastest Door Delivery"} />
+                    <ServiceItemCardComponent imageSRC={"/img/service-2.jpg"} iconName={FaCoffee} serviceTitle={"Fresh Coffee Beans"} />
+                    <ServiceItemCardComponent imageSRC={"/img/service-3.jpg"} iconName={FaAward} serviceTitle={"Best Quality Coffee"} />
+                    <ServiceItemCardComponent imageSRC={"/img/service-4.jpg"} iconName={RiCalendarScheduleFill} serviceTitle={"Online Table Booking"} />
                 </div>
             </div>
         </div>
