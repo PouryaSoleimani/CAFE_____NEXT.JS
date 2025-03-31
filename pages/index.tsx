934737d451a17a490691bf7adf47913f52f65bf8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-31 19:15:19 +0330

    RESERVATION

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import ServicesTemplate from "@/components/templates/HomePage/ServicesTemplate";
 import { GetStaticProps } from "next";
 import OfferTemplate from "@/components/templates/HomePage/OfferTemplate";
 import MenuTemplate from "@/components/templates/HomePage/MenuTemplate";
+import ReservationTemplate from "@/components/templates/HomePage/ReservationTemplate";
 
 interface ServicesTemplateProps { data: [{ serviceID: number, serviceTitle: string, description: string, imgSRC: string, iconName: React.ElementType }], menuData: [{ id: number, name: string, imgSRC: string, icon: string, price: number | string, description: string }] }
 
@@ -23,69 +24,7 @@ const HomePage: React.FC<ServicesTemplateProps> = ({ data, menuData }) => {
       <ServicesTemplate data={data} />
       <OfferTemplate />
       <MenuTemplate menuData={menuData} />
-
-
-      {/* <!-- Reservation Start --> */}
-      <div className="container-fluid my-5">
-        <div className="container">
-          <div className="reservation position-relative overlay-top overlay-bottom">
-            <div className="row align-items-center">
-              <div className="col-lg-6 my-5 my-lg-0">
-                <div className="p-5">
-                  <div className="mb-4">
-                    <h1 className="display-3 text-primary">30% OFF</h1>
-                    <h1 className="text-white">For Online Reservation</h1>
-                  </div>
-                  <p className="text-white">Lorem justo clita erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                    ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea</p>
-                  <ul className="list-inline text-white m-0">
-                    <li className="py-2"><span className="fa fa-check text-primary mr-3"></span> Lorem ipsum dolor sit amet</li>
-                    <li className="py-2"><span className="fa fa-check text-primary mr-3"></span> Lorem ipsum dolor sit amet</li>
-                    <li className="py-2"><span className="fa fa-check text-primary mr-3"></span> Lorem ipsum dolor sit amet</li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="text-center p-5" style={{ background: "rgba(51, 33, 29, .8)" }}>
-                  <h1 className="text-white mb-4 mt-5">Book Your Table</h1>
-                  <form className="mb-5">
-                    <div className="form-group">
-                      <input type="text" className="form-control bg-transparent border-primary p-4" placeholder="Name" required />
-                    </div>
-                    <div className="form-group">
-                      <input type="email" className="form-control bg-transparent border-primary p-4" placeholder="Email" required />
-                    </div>
-                    <div className="form-group">
-                      <div className="date" id="date" data-target-input="nearest">
-                        <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Date" data-target="#date" data-toggle="datetimepicker" />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <div className="time" id="time" data-target-input="nearest">
-                        <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker" />
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <select className="custom-select bg-transparent border-primary px-4" style={{ height: "49px" }}>
-                        <option defaultValue={0}> Person</option>
-                        <option value="1">Person 1</option>
-                        <option value="2">Person 2</option>
-                        <option value="3">Person 3</option>
-                        <option value="3">Person 4</option>
-                      </select>
-                    </div>
-
-                    <div>
-                      <button className="btn btn-primary btn-block font-weight-bold py-3" type="submit">Book Now</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- Reservation End --> */}
+      <ReservationTemplate />
 
 
       {/* <!-- Testimonial Start --> */}
