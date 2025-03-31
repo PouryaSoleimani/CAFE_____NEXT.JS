cde52a297c55d7f1319924fe8b84591c3ac6856c
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-31 16:54:09 +0330

    MENU

--- components/templates/HomePage/MenuTemplate.tsx
+++ components/templates/HomePage/MenuTemplate.tsx
@@ -10,7 +10,7 @@ const MenuTemplate: React.FC<MenuTemplateProps> = ({ menuData }) => {
         <div className="container-fluid pt-5">
             <div className="container">
                 <div className="section-title text-center mb-5">
-                    <h4 className="text-[#da9f5b] text-uppercase tracking-widest">Menu &amp; Pricing</h4>
+                    <h1 className="text-[#da9f5b] text-uppercase tracking-widest">Menu &amp; Pricing</h1>
                     <h1 className="display-4 text-white font-bold">Competitive Pricing</h1>
                 </div>
                 <div className="row flex flex-wrap items-center justify-center lg:gap-x-10">
