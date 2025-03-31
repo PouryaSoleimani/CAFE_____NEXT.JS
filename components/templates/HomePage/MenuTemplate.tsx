d26d1734b13e38e14e83c57a722d4a3432f210be
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-31 16:47:02 +0330

    MENU

--- components/templates/HomePage/MenuTemplate.tsx
+++ components/templates/HomePage/MenuTemplate.tsx
@@ -2,6 +2,9 @@ import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import React from 'react'
 interface MenuTemplateProps { menuData: [{ id: number, name: string, imgSRC: string, icon: string, price: number | string, description: string }] }
+type SingleMenuItem = { id: number, name: string, imgSRC: string, icon: string, price: number | string, description: string }
+
+// COMPONENT ===============================================================================================================================================================================
 const MenuTemplate: React.FC<MenuTemplateProps> = ({ menuData }) => {
     return (
         <div className="container-fluid pt-5">
@@ -10,85 +13,21 @@ const MenuTemplate: React.FC<MenuTemplateProps> = ({ menuData }) => {
                     <h4 className="text-[#da9f5b] text-uppercase tracking-widest">Menu &amp; Pricing</h4>
                     <h1 className="display-4 text-white font-bold">Competitive Pricing</h1>
                 </div>
-                <div className="row flex items-center justify-center lg:gap-x-10">
-                    <div className="col-lg-5">
-                        <h1 className="mb-5 text-[#da9f5b] font-bold">Hot Coffee</h1>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
-                                <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-1.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$5</h5>
-                            </div>
-                            <div className="col-8 col-sm-9">
-                                <h4 className="text-white font-bold">Black Coffee</h4>
-                                <p className="m-0 text-gray-400">
-                                    Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
-                                </p>
-                            </div>
-                        </div>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
+                <div className="row flex flex-wrap items-center justify-center lg:gap-x-10">
+                    {menuData.map((menuItem: SingleMenuItem) => (
+                        <div className="menuITemBox  align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
+                            <div className="w-32 h-32 relative mx-auto">
                                 <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-2.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$7</h5>
+                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2 absolute top-3 -right-10">$7</h5>
                             </div>
-                            <div className="col-8 col-sm-9">
+                            <div className="">
                                 <h4 className="text-white font-bold">Chocolate Coffee</h4>
                                 <p className="m-0 text-gray-400">
                                     Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
                                 </p>
                             </div>
                         </div>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
-                                <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-3.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$9</h5>
-                            </div>
-                            <div className="col-8 col-sm-9">
-                                <h4 className="text-white font-bold">Coffee With Milk</h4>
-                                <p className="m-0 text-gray-400">
-                                    Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-5">
-                        <h1 className="mb-5 text-[#da9f5b] font-bold">Cold Coffee</h1>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
-                                <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-1.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$5</h5>
-                            </div>
-                            <div className="col-8 col-sm-9">
-                                <h4 className="text-white font-bold">Black Coffee</h4>
-                                <p className="m-0 text-gray-400">
-                                    Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
-                                </p>
-                            </div>
-                        </div>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
-                                <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-2.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$7</h5>
-                            </div>
-                            <div className="col-8 col-sm-9">
-                                <h4 className="text-white font-bold">Chocolate Coffee</h4>
-                                <p className="m-0 text-gray-400">
-                                    Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
-                                </p>
-                            </div>
-                        </div>
-                        <div className="menuITemBox row align-items-center mb-5 hover:scale-105 transition-transform duration-300 ease-in-out">
-                            <div className="col-4 col-sm-3">
-                                <Image className="w-100 rounded-full shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src="/img/menu-3.jpg" alt="pic" width={100} height={100} />
-                                <h5 className="menu-price text-[#da9f5b] font-semibold mt-2">$9</h5>
-                            </div>
-                            <div className="col-8 col-sm-9">
-                                <h4 className="text-white font-bold">Coffee With Milk</h4>
-                                <p className="m-0 text-gray-400">
-                                    Sit lorem ipsum et diam elitr est dolor sed duo guberg sea et et lorem dolor
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
@@ -105,3 +44,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
 
 export default MenuTemplate
+
+
+
+
