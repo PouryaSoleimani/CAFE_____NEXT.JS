746f19fb8e6bbbebc8ae769397d1f75b1389d454
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-03-31 17:46:11 +0330

    MENU

--- components/modules/MenuItem/MenuItemCardComponent.tsx
+++ components/modules/MenuItem/MenuItemCardComponent.tsx
@@ -6,13 +6,13 @@ interface MenuItemProps { id: number, name: string, imgSRC: string, price: numbe
 const MenuItemCardComponent: React.FC<MenuItemProps> = ({ id, name, imgSRC, price, description }) => {
     return (
         <div key={id} className="menuITemBox hover:scale-105 transition-transform duration-300 ease-in-out">
-            <div className="w-44 h-44 relative mx-auto">
-                <Image className="w-72 h-52 rounded-md shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src={imgSRC} alt="pic" width={200} height={300} />
+            <div className="w-64 h-52 relative mx-auto">
+                <Image className="rounded-md h-52 shadow-lg hover:shadow-[#da9f5b] transition-shadow duration-300" src={imgSRC} alt="pic" width={200} height={300} />
                 <h5 className="menu-price text-[#da9f5b] font-extrabold mt-2 absolute -top-4 -left-10">${price}</h5>
             </div>
             <div className="">
                 <h4 className="text-white font-bold" style={{ fontSize: "2.2rem" }}>{name}</h4>
-                <p className="m-0 text-gray-400 w-fit text-2xl font-extralight">
+                <p className="m-0 text-gray-400 w-fit text-2xl font-extralight whitespace-nowrap">
                     {description}
                 </p>
             </div>
