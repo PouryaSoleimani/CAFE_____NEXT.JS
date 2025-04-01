a7bacb24c3d0e90ac023dbf29880927e0ac82241
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-01 16:17:55 +0330

    RESERVATION FORM

--- components/modules/ReservationForm/ReservationFormComponent.tsx
+++ components/modules/ReservationForm/ReservationFormComponent.tsx
@@ -22,47 +22,43 @@ const ReservationFormComponent = () => {
 
     const { register, handleSubmit, watch, formState: { errors }, } = useForm<Inputs | any>({ resolver: yupResolver(schema), });
 
-    const onSubmit: SubmitHandler<Inputs | any> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<Inputs | any> = (data) => console.info(data)
 
     return (
         <div className="col-lg-6">
             <div className="text-center p-5" style={{ background: "rgba(51, 33, 29, .8)" }}>
                 <h1 className="text-white mb-4 mt-5">Book Your Table</h1>
-                <form className="mb-5" onSubmit={handleSubmit(onSubmit)}>
+                <form className="mb-5  flex flex-col gap-y-5" onSubmit={handleSubmit(onSubmit)}>
 
                     <div className="form-group">
-                        <input type="text" className="form-control bg-transparent border-primary p-4" placeholder="Name" {...register('name')} />
-                        {errors.name && <span>Invalid Value</span>}
+                        <input type="text" className="form-control bg-transparent border-primary p-4 text-zinc-200" placeholder="Name" {...register('name')} />
+                        {errors.name && <span className='text-white bg-red-500/30 px-2 py-1 my-2 w-full'>Invalid Value</span>}
                     </div>
                     <div className="form-group">
-                        <input type="email" className="form-control bg-transparent border-primary p-4" placeholder="Email"  {...register('email')} />
-                        {errors.email && <span>Invalid Value</span>}
+                        <input type="email" className="form-control bg-transparent border-primary p-4 text-zinc-200" placeholder="Email"  {...register('email')} />
+                        {errors.email && <span className='text-white bg-red-500/30 px-2 py-1 my-2 w-full'>Invalid Value</span>}
                     </div>
                     <div className="form-group">
-                        <div className="date" id="date" data-target-input="nearest">
-                            <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Date" data-target="#date" data-toggle="datetimepicker" {...register("date")} />
-                            {errors.data && <span>Invalid Value</span>}
-                        </div>
+                        <input type="text" className="form-control bg-transparent border-primary p-4 text-zinc-200 datetimepicker-input" placeholder="Date" data-target="#date" data-toggle="datetimepicker" {...register("date")} />
+                        {errors.date && <span className='text-white bg-red-500/30 px-2 py-1 my-2 w-full'>Invalid Value</span>}
                     </div>
                     <div className="form-group">
-                        <div className="time" id="time" data-target-input="nearest">
-                            <input type="text" className="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker" {...register("time")} />
-                            {errors.time && <span>Invalid Value</span>}
-                        </div>
+                        <input type="text" className="form-control bg-transparent border-primary p-4 text-zinc-200 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker" {...register("time")} />
+                        {errors.time && <span className='text-white bg-red-500/30 px-2 py-1 my-2 w-full'>Invalid Value</span>}
                     </div>
 
                     <div className="form-group">
-                        <select className="custom-select bg-transparent border-primary px-4" style={{ height: "49px" }} {...register("person")}>
+                        <select className="custom-select bg-transparent border-primary px-4 text-zinc-200" style={{ height: "49px" }} {...register("person")}>
                             <option defaultValue={0}> Person</option>
                             <option value="1">Person 1</option>
                             <option value="2">Person 2</option>
                             <option value="3">Person 3</option>
                             <option value="3">Person 4</option>
                         </select>
-                        {errors.person && <span>Invalid Value</span>}
+
                     </div>
 
-                    <div>
+                    <div className='mt-5'>
                         <button className="btn btn-primary btn-block font-weight-bold py-3" type="submit">Book Now</button>
                     </div>
 
