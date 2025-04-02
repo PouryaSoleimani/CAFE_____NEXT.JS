cd86256cd470bc7a7e8edf2c06c056be71e47ce1
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 18:54:42 +0330

    CONTACT-US

--- components/templates/ContactUsPage/ContactUsDetails.tsx
+++ components/templates/ContactUsPage/ContactUsDetails.tsx
@@ -69,7 +69,7 @@ const ContactUsDetails = () => {
                 <div className="row">
                     {/* IFRAME */}
                     <div className="col-md-6 pb-5">
-                        <iframe style={{ width: "100%", height: "443px", border: "0" }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameBorder="0" allowFullScreen aria-hidden="false" tabIndex={0}  ></iframe>
+                        <iframe style={{ width: "100%", height: "448px", border: "0" }} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameBorder="0" allowFullScreen aria-hidden="false" tabIndex={0}  ></iframe>
                     </div>
 
                     {/* FORM */}
@@ -93,7 +93,7 @@ const ContactUsDetails = () => {
                                     <p className="help-block text-danger"></p>
                                 </div>
                                 <div className="control-group">
-                                    <textarea className="form-control text-white   py-3 px-4" rows={5} id="message" placeholder="Message" required data-validation-required-message="Please enter your message"  {...register("message")}   ></textarea>
+                                    <textarea className="form-control text-white   py-3 px-4" rows={10} id="message" placeholder="Message" required data-validation-required-message="Please enter your message"  {...register("message")}   ></textarea>
                                     {errors.message && <p className='text-white bg-red-500/30 px-2 py-1 my-2 w-full'>Invalid Value</p>}
                                     <p className="help-block text-danger"></p>
                                 </div>
