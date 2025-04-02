89e794f10ded13b7a17eca88605c932646298521
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 15:40:23 +0330

    COMMENTS

--- components/modules/Footer/FooterForm.tsx
+++ components/modules/Footer/FooterForm.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { useForm, SubmitHandler } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import axios from 'axios'
+import toast from 'react-hot-toast';
+type Inputs = { email: string }
 
+
+//  COMPONENT =====================================================================================================================================================
 const FooterForm = () => {
+
+
+    const schema = yup.object().shape({ email: yup.string().email().required(), }).required();
+    const { register, handleSubmit, formState: { errors }, resetField } = useForm<Inputs>({ resolver: yupResolver(schema), })
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        axios.post("http://localhost:4000/newsLetterUsers", { data })
+            .then(response => {
+                console.info(response);
+                toast.success('Email Succesfully Signed Up !')
+                resetField("email")
+            })
+            .catch(err => {
+                console.info("ERROR ==>", err);
+                toast.error("Unknown Error")
+            })
+    }
     return (
         <form className="w-100">
             <div className="input-group">
