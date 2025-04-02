b75a5bac00d10a82310d1f3e067e8254dbb597cc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 15:38:17 +0330

    FOOTER

--- components/modules/Footer/FooterComponent.tsx
+++ components/modules/Footer/FooterComponent.tsx
@@ -46,7 +46,7 @@ const FooterComponent = () => {
                 </div>
             </div>
             <div className="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style={{ borderColor: " rgba(256, 256, 256, .1) !important" }}>
-                <p className="mb-2 text-white">Copyright &amp;copy; <Link className="font-weight-bold" href="/">Domain</Link> . All Rights Reserved</p>
+                <p className="mb-2 text-white">Created By <span className='text-[#da9f5b] font-bold'>Pourya Soleimani</span> | 2025  . All Rights Reserved</p>
             </div>
         </div >
     )
