d2673a93e9bf258fd3edfec9cab2b6a0a3acc550
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 18:30:40 +0330

    CONTACT US

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,11 +1,12 @@
 import PageHeaderComponent from '@/components/modules/PageHeader/PageHeaderComponent'
+import ContactUsDetails from '@/components/templates/ContactUsPage/ContactUsDetails'
 import React from 'react'
 
 const ContactUsPage = () => {
     return (
         <>
             <PageHeaderComponent route={"CONTACT US PAGE"} />
-        
+            <ContactUsDetails />
         </>
     )
 }
