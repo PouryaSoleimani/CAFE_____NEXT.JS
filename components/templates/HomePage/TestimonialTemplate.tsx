4bd31bfc28cd023c550c860896726a59356552fe
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 13:28:41 +0330

    COMMENTS

--- components/templates/HomePage/TestimonialTemplate.tsx
+++ components/templates/HomePage/TestimonialTemplate.tsx
@@ -16,12 +16,12 @@ const TestimonialTemplate: React.FC<commentsProps> = ({ comments }) => {
             <div className="container">
                 <div className="section-title">
                     <h4 className="text-primary text-uppercase" style={{ letterSpacing: "5px" }}>Testimonial</h4>
-                    <h1 className="display-4">Our Clients Say</h1>
+                    <h1 className="display-4">What Do Our Clients Say</h1>
                 </div>
                 <div id="SWIPER___CONTAINER" className='flex items-center justify-center w-full px-10 bg-zinc-900/30 py-5 rounded-md border-2 border-zinc-700'>
                     <Swiper grabCursor={true} rewind loop scrollbar autoplay className="mySwiper mx-auto" width={300} height={270}>
                         {comments?.map((comment: SingleCommentType) => (
-                            <SwiperSlide className='bg-zinc-900/70 w-[10rem] flex swiper____slide rounded-md'>
+                            <SwiperSlide key={comment.id} className='bg-zinc-900/70 w-[10rem] flex swiper____slide rounded-md'>
                                 <Image className="w-full h-2/3 rounded-t-md shadow shadow-amber-50" src={comment.imgSRC} alt="pic" width={800} height={800} loading='eager' />
                                 <div className="swiper__contents px-10 mt-10 text-center whitespace-nowrap">
                                     <h4>{comment.username.toUpperCase()}</h4>
