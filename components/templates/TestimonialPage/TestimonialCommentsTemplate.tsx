dac23b498094569dbc9b284cb2c7403b29e6eac3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 18:03:29 +0330

    MENU PAGE

--- components/templates/TestimonialPage/TestimonialCommentsTemplate.tsx
+++ components/templates/TestimonialPage/TestimonialCommentsTemplate.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+//  TESTEMONIAL TEMPLATE ================================================================================================================================================================
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image'
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+interface commentsProps { comments: [{ id: number | string, username: string, body: string, imgSRC: string, productID: string }] }
+type SingleCommentType = { id: number | string, username: string, body: string, imgSRC: string, productID: string }
 
-const TestimonialCommentsTemplate = () => {
+//  COMPONENT ===================================================================================================================================================================================
+const TestimonialCommentsTemplate: React.FC<commentsProps> = ({ comments }) => {
+
     return (
-        <div>TestimonialCommentsTemplate</div>
+        <div className="container-fluid py-5">
+            <div className="container">
+                <div className="section-title">
+                    <h4 className="text-primary text-uppercase" style={{ letterSpacing: "5px" }}>Testimonial</h4>
+                    <h1 className="display-4">What Do Our Clients Say</h1>
+                </div>
+                <div id="SWIPER___CONTAINER" className='flex items-center justify-center w-full px-10 bg-zinc-900/30 py-5 rounded-md border-2 border-zinc-700'>
+                    <Swiper grabCursor={true} rewind loop scrollbar autoplay className="mySwiper mx-auto" width={300} height={270}>
+                        {comments?.map((comment: SingleCommentType) => (
+                            <SwiperSlide key={comment.id} className='bg-zinc-900/70 w-[10rem] flex swiper____slide rounded-md'>
+                                <Image className="w-full h-2/3 rounded-t-md shadow shadow-amber-50" src={comment.imgSRC} alt="pic" width={800} height={800} loading='eager' />
+                                <div className="swiper__contents px-10 mt-10 text-center whitespace-nowrap">
+                                    <h4>{comment.username.toUpperCase()}</h4>
+                                    <span className='text-xl mt-2 font-semibold text-white'>{comment.body}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+        </div>
     )
 }
+
+
+
 
 export default TestimonialCommentsTemplate
--- pages/testimonial/index.tsx
+++ pages/testimonial/index.tsx
@@ -1,9 +1,28 @@
+import PageHeaderComponent from '@/components/modules/PageHeader/PageHeaderComponent'
+import TestimonialCommentsTemplate from '@/components/templates/TestimonialPage/TestimonialCommentsTemplate'
+import { GetStaticProps } from 'next'
 import React from 'react'
 
-const TestimonialPage = () => {
+interface TestimonialPageProps {
+    comments: [{ id: number | string, username: string, body: string, imgSRC: string, productID: string }]
+
+}
+const TestimonialPage: React.FC<TestimonialPageProps> = ({ comments }) => {
     return (
-        <div>TestimonialPage</div>
+        <>
+            <PageHeaderComponent route={"TESTIMONIAL PAGE"} />
+            <TestimonialCommentsTemplate comments={comments} />
+        </>
     )
 }
+// GET STATIC PROPS =================================================================================================================
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const commentsReq = await fetch("http://localhost:4000/comments")
+    const comments = await commentsReq.json()
 
+    return {
+        props: { comments: comments },
+        revalidate: 43200
+    }
+}
 export default TestimonialPage
