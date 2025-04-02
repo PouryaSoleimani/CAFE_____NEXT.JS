c6881dc416ed544c1789a073eee2491e5c1cfaca
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 15:43:02 +0330

    COMMENTS

--- components/modules/Footer/FooterComponent.tsx
+++ components/modules/Footer/FooterComponent.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 import { FaEnvelope, FaFacebookSquare, FaLinkedin, FaMapMarkerAlt, FaPhone, FaTwitter } from 'react-icons/fa'
 import { FaSquareInstagram } from 'react-icons/fa6'
 import FooterForm from './FooterForm'
+import FooterCopyRight from './FooterCopyRight'
 
 const FooterComponent = () => {
     return (
         <div className="container-fluid footer text-white mt-5 pt-5 px-0 position-relative overlay-top">
             <div className="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
+                {/* GET IN TOUCH */}
                 <div className="col-lg-3 col-md-6 mb-5">
                     <h4 className="text-white text-uppercase mb-4 bg-zinc-600/20 w-fit p-2 rounded-sm" style={{ letterSpacing: "3px" }}>Get In Touch</h4>
                     <p className="flex items-center"><span className="fa fa-map-marker-alt mr-2 "><FaMapMarkerAlt className='text-red-900 w-5 h-5' /></span> 123 Street, New York, USA</p>
                     <p className="flex items-center"><span className="fa fa-phone-alt mr-2 "><FaPhone className='text-blue-900 w-4 h-4' /></span> +012 345 67890</p>
                     <p className="m-0 flex items-center"><span className="fa fa-envelope mr-2 "><FaEnvelope className='text-green-900' /></span> info@example.com</p>
                 </div>
+                {/* FOLLOW US */}
                 <div className="col-lg-3 col-md-6 mb-5">
                     <h4 className="text-white text-uppercase mb-4 mt-1 bg-zinc-600/20 w-fit p-2 rounded-sm" style={{ letterSpacing: "3px" }}>Follow Us</h4>
                     <p>Follow us on different Social Medias to keep in touch </p>
@@ -24,6 +27,7 @@ const FooterComponent = () => {
                         <Link href="/"><FaSquareInstagram className="w-12 h-12 mx-2  hover:-translate-y-2 duration-200 text-violet-900" /> </Link>
                     </div>
                 </div>
+                {/* OPEN HOURS */}
                 <div className="col-lg-3 col-md-6 mb-5">
                     <h4 className="text-white text-uppercase mb-4  mt-1 bg-zinc-600/20 w-fit p-2 rounded-sm" style={{ letterSpacing: "3px" }}>Open Hours</h4>
                     <div>
@@ -33,15 +37,14 @@ const FooterComponent = () => {
                         <p className='text-zinc-400 tracking-widest'>2.00 PM - 8.00 PM</p>
                     </div>
                 </div>
+                {/* NEWSLETTER */}
                 <div className="col-lg-3 col-md-6 mb-5">
                     <h4 className="text-white text-uppercase mb-4  mt-1 bg-zinc-600/20 w-fit p-2 rounded-sm" style={{ letterSpacing: "3px" }}>Newsletter</h4>
                     <p>Join Our Newsletter to Get Our News and Offers</p>
                     <FooterForm />
                 </div>
             </div>
-            <div className="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style={{ borderColor: " rgba(256, 256, 256, .1) !important" }}>
-                <p className="mb-2 text-white">Created By <span className='text-[#da9f5b] font-bold'>Pourya Soleimani</span> | 2025  . All Rights Reserved</p>
-            </div>
+            <FooterCopyRight />
         </div >
     )
 }
---
+++ components/modules/Footer/FooterCopyRight.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterCopyRight = () => {
+    return (
+        <div className="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style={{ borderColor: " rgba(256, 256, 256, .1) !important" }}>
+            <p className="mb-2 text-white">Created By <span className='text-[#da9f5b] font-bold'>Pourya Soleimani</span> | 2025  . All Rights Reserved</p>
+        </div>
+    )
+}
+
+export default FooterCopyRight
