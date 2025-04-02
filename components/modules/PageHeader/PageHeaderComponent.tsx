d55b86fdf1b1a6484b627395a44af30479f8d5f9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 14:57:29 +0330

    COMMENTS

--- components/modules/PageHeader/PageHeaderComponent.tsx
+++ components/modules/PageHeader/PageHeaderComponent.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
-const PageHeaderComponent = () => {
+const PageHeaderComponent = (route: any) => {
     return (
-        <div>PageHeaderComponent</div>
+        <div className="container-fluid page-header mb-5 position-relative overlay-bottom">
+            <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{ minHeight: "400px" }}>
+                <h1 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">{String(route.route)}</h1>
+                <div className="d-inline-flex mb-lg-5">
+                    <Link href="/" className="m-0 text-white"> Home </Link>
+                    <p className="m-0 text-white px-2">/</p>
+                    <p className="m-0 text-white">{String(route.route)}</p>
+                </div>
+            </div >
+        </div >
     )
 }
 
--- components/templates/AboutUsPage/AboutUsPage.tsx
+++ components/templates/AboutUsPage/AboutUsPage.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import AboutUsTemplate from '../HomePage/AboutUsTemplate'
+import PageHeaderComponent from '@/components/modules/PageHeader/PageHeaderComponent'
+import { useRouter } from 'next/router'
 
 const AboutUsPage = () => {
+
     return (
         <>
+            <PageHeaderComponent route={"ABOUT US"} />
             <AboutUsTemplate />
         </>
     )
