785da501ab24f0ca4c01b0a4f996c94832c916bf
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-02 18:39:43 +0330

    CONTACT-US

--- components/templates/ContactUsPage/ContactUsDetails.tsx
+++ components/templates/ContactUsPage/ContactUsDetails.tsx
@@ -44,19 +44,19 @@ const ContactUsDetails = () => {
                             <div id="success"></div>
                             <form name="sentMessage" id="contactForm" noValidate>
                                 <div className="control-group">
-                                    <input type="text" className="form-control bg-transparent p-4" id="name" placeholder="Your Name" required data-validation-required-message="Please enter your name" />
+                                    <input type="text" className="form-control p-4" id="name" placeholder="Your Name" required data-validation-required-message="Please enter your name" />
                                     <p className="help-block text-danger"></p>
                                 </div>
                                 <div className="control-group">
-                                    <input type="email" className="form-control bg-transparent p-4" id="email" placeholder="Your Email" required data-validation-required-message="Please enter your email" />
+                                    <input type="email" className="form-control p-4" id="email" placeholder="Your Email" required data-validation-required-message="Please enter your email" />
                                     <p className="help-block text-danger"></p>
                                 </div>
                                 <div className="control-group">
-                                    <input type="text" className="form-control bg-transparent p-4" id="subject" placeholder="Subject" required data-validation-required-message="Please enter a subject" />
+                                    <input type="text" className="form-control p-4" id="subject" placeholder="Subject" required data-validation-required-message="Please enter a subject" />
                                     <p className="help-block text-danger"></p>
                                 </div>
                                 <div className="control-group">
-                                    <textarea className="form-control bg-transparent py-3 px-4" rows={5} id="message" placeholder="Message" required data-validation-required-message="Please enter your message"    ></textarea>
+                                    <textarea className="form-control  py-3 px-4" rows={5} id="message" placeholder="Message" required data-validation-required-message="Please enter your message"    ></textarea>
                                     <p className="help-block text-danger"></p>
                                 </div>
                                 <div>
