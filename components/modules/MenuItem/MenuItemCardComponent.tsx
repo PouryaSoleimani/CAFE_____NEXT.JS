b25ba34e5f37a74a9430c3ff4880c073d38a99ea
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-03 15:40:22 +0330

    SEARCH

--- components/modules/MenuItem/MenuItemCardComponent.tsx
+++ components/modules/MenuItem/MenuItemCardComponent.tsx
@@ -10,7 +10,7 @@ const MenuItemCardComponent: React.FC<MenuItemProps> = ({ id, name, imgSRC, pric
 
         <div key={id} className="menuITemBox">
             <div className="w-60 h-52 relative mx-auto">
-                <Image className="rounded-md h-52" src={imgSRC} alt="pic" width={200} height={300} />
+                <Image className="rounded-md h-52" src={imgSRC} alt="pic" width={400} height={300} />
                 <h5 className="menu-price text-[#da9f5b] font-extrabold mt-2 absolute -top-4 -left-8 px-2">${price}</h5>
             </div>
             <div className="">
