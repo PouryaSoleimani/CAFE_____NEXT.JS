dd055e21d50ca8d6377e5454474672388150b031
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-04 13:40:13 +0330

    REFACTOR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { ThemeModeScript } from 'flowbite-react';
+
 
 export default function Document() {
   return (
@@ -9,7 +9,6 @@ export default function Document() {
         <meta name="description" content="Next-Coffee - A Next.js Coffee Shop Template" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../path/to/flowbite/dist/flowbite.min.css" />
-        <ThemeModeScript localStorageKey="theme" mode="dark" defaultMode="auto" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.4.0/flowbite.min.css" />
