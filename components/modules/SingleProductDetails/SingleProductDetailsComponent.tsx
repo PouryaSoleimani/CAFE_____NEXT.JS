25ce239523a149b0657efc23458b15650a38fcd8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-04 11:43:29 +0330

    SINGLE PRODUCT DETAILS

--- components/modules/SingleProductDetails/SingleProductDetailsComponent.tsx
+++ components/modules/SingleProductDetails/SingleProductDetailsComponent.tsx
@@ -21,25 +21,32 @@ const SingleProductDetailsComponent: React.FC<SingleProductDetailsPageTemplatePr
             {singleProduct ? (
                 <div className='flex flex-col items-center justify-center gap-y-2'>
                     <div className='bg-zinc-800/50 p-5 rounded-sm flex flex-col items-center gap-y-5 justify-center border border-zinc-600'>
+
                         <Image src={singleProduct?.imgSRC} width={300} height={700} alt='pic' />
+
                         <div className='flex gap-x-1'>
                             {Array.from({ length: singleProduct.rate }, (_, i) => (<span key={i}><RiStarFill className='text-2xl text-yellow-400' /></span>))}
                             {Array.from({ length: 5 - singleProduct.rate }, (_, i) => (<span key={i}><BiStar className='text-2xl text-yellow-400' /></span>))}
                         </div>
+
                         <h1 className='text-white underline decoration-amber-800'>{singleProduct.name}</h1>
                         <h3 className='bg-black px-3 py-1 text-white rounded-sm'>PRICE : ${singleProduct.price}</h3>
                         <p className='text-xl text-zinc-500'>{singleProduct.description}</p>
+
                     </div>
+
                     <button className="flex items-center justify-center gap-x-5 px-2 bg-black p-4 text-white border text-3xl font-black hover:bg-zinc-900 duration-300 w-full mx-auto" onClick={backToMenuHandler}  >
                         <ArrowLeft /> BACK TO MENU
                     </button>
+
                 </div>
+
             ) : (
+                // LOADING ______________________________________________________________________________________________________________________________________________________
                 <div className='w-full h-full flex items-center justify-center'>
                     <h1>LOADING ...</h1>
                 </div>
             )}
-
         </div>
     )
 }
