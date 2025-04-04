c748a325e7072a6df56b4d95f4f6b339d12bf3fd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-04 13:42:04 +0330

    REFACTOR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,16 +8,11 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Next-Coffee - A Next.js Coffee Shop Template" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="../path/to/flowbite/dist/flowbite.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.4.0/flowbite.min.css" />
       </Head>
       <title>☕Next-Coffee</title>
       <body className="antialiased">
         <Main />
         <NextScript />
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
       </body>
     </Html>
   );
